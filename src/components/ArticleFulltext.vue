0e5672320df8526d9dd4b47c08eaddbe33d42362
Dmitry Aronov <aronov.mml@gmail.com>
2023-12-25 20:35:33 +0700

    added hide/reveal button for ArticleFulltext component

--- src/components/ArticleFulltext.vue
+++ src/components/ArticleFulltext.vue
@@ -13,34 +13,40 @@
     </div>
     <div class="article__callout">
       <div class="article__recommended">{{ article.content.recommended }}</div>
-      <ul class="article__tags"> 
+      <ul class="article__tags">
         <li v-for="tag in article.tags" :key="tag" class="article__tag">
           {{ tag }}
         </li>
       </ul>
     </div>
-    <div v-for="block in article.content.body" :key="block._id" class="article__body">
-      <section class="article-block">
-        <h2 class="article-block__header">{{ block.subTitle }}</h2>
-        <p class="article-block__text">{{ block.subTitleText }}</p>
-      </section>
+    <div @click="isBodyHidden = !isBodyHidden" class="article__hide">
+      <Icon icon="material-symbols:collapse-content" width="30" height="30" v-if="!isBodyHidden" />
+      <Icon icon="material-symbols:expand-content" width="30" height="30" v-else />
+      <span>{{ isBodyHidden ? 'Показать' : 'Скрыть'}}</span>
+
     </div>
+    <TransitionGroup name="body">
+      <div id="article-body-container" v-if="!isBodyHidden">
+        <div v-for="block in article.content.body" :key="block._id" class="article__body">
+          <section class="article-block">
+            <h2 class="article-block__header">{{ block.subTitle }}</h2>
+            <p class="article-block__text">{{ block.subTitleText }}</p>
+          </section>
+        </div>
+      </div>
     <h2 class="article__header--tasks">Задания:</h2>
     <div class="article__footer">
       <my-button>Поделиться</my-button>
       <!-- here will be a 3d party library like https://www.growthbunker.dev/vuesocial/ -->
       <my-button>В избранное</my-button> <!-- here will be a UI component -->
-      <my-button>Скрыть</my-button> <!-- here will be a UI component -->
     </div>
-    <div class="article__tasks">
-      <!-- <ol>
-        <li></li>
-      </ol> -->
-      <div v-for="(task, index) in article.content.tasks" :key="task._id" class="article-task">
+    <ol class="article__tasks">
+      <li v-for="(task, index) in article.content.tasks" :key="task._id" class="article-task">
         <h3 class="article-task__header">{{ index + 1 }}. {{ task.taskTitle }}</h3>
         <p class="article-task__description">{{ task.taskText }}</p>
-      </div>
-    </div>
+      </li>
+    </ol>
+  </TransitionGroup>
   </article>
 </template>
 
@@ -60,6 +66,7 @@ const props = defineProps({
 
 const article = ref(ArticleMockup); // will be computed or onMounted fetch
 const author = ref(UsersMockup[0]); // will be computed or onMounted fetch
+const isBodyHidden = ref(false);
 
 const fetchArticle = async () => {
   try {
@@ -166,6 +173,24 @@ const updatedAt = publishedString(article.value.updatedAt);
     font-size: 12px;
   }
 
+  &__hide {
+    margin-bottom: 20px;
+    display: flex;
+    gap: 5px;
+    align-items: center;
+
+    & svg {
+      display: inline;
+      transform: translateY(-1px);
+    }
+
+    & span {
+      font-size: 20px;
+      color: #666;
+      border-bottom: 1px dotted #666;
+    }
+  }
+
   &__body {
     margin-bottom: 35px;
   }
@@ -193,6 +218,7 @@ const updatedAt = publishedString(article.value.updatedAt);
     padding: 25px 30px;
     margin-bottom: 15px;
     background-color: white;
+    list-style: none;
   }
 
   .article-task {
@@ -211,9 +237,29 @@ const updatedAt = publishedString(article.value.updatedAt);
   }
 
   &__footer {
+    margin-bottom: 20px;
     display: flex;
     gap: 20px;
 
   }
 }
+
+// animations
+.body-move,
+.body-enter-active,
+.body-leave-active {
+  transition: all .6s ease-in-out;
+}
+
+.body-leave-active {
+  position: absolute;
+  max-width: 1120px; // a crutch for TransitionGroup. Without it the text disappears in an ugly way
+}
+
+.body-enter-from,
+.body-leave-to {
+  opacity: 0;
+  transform: translateY(-30px);
+}
+
 </style>
