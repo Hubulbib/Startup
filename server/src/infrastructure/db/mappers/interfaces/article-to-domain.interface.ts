622cc7ef62010cdb7a8692ce65f4c224bc2dd034
Kamal <kamal.kurbanov2018@gmail.com>
2024-01-25 14:18:56 +0300

    refac: now article.author is UserEntity object

--- server/src/infrastructure/db/mappers/interfaces/article-to-domain.interface.ts
+++ server/src/infrastructure/db/mappers/interfaces/article-to-domain.interface.ts
@@ -1,5 +1,7 @@
 import { type IArticleDoc } from '../../entities/article.entity'
+import { UserEntity } from '../../../../core/entites/user.entity'
 
 export interface IArticleToDomain extends IArticleDoc {
+  authorData: UserEntity
   mentors: string[]
 }
