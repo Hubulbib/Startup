8cdacb05c3abbcc40606891f67eb8c75629d92dc
Marat-Akhalov <117514563+Marat-Akhalov@users.noreply.github.com>
2024-03-04 11:42:07 +0400

    feat: add more svg icons

--- client/src/assets/svg/index.js
+++ client/src/assets/svg/index.js
@@ -4,12 +4,18 @@ import battery from '@/assets/svg/battery.vue';
 import star from '@/assets/svg/star.vue';
 import arrowLeft from '@/assets/svg/arrow-left.vue';
 import themeSwitcher from '@/assets/svg/theme-switcher.vue';
+import favorite from '@/assets/svg/favorite.vue';
+import dummyUser from '@/assets/svg/dummy-user.vue';
+import hide from '@/assets/svg/hide.vue';
 
 export default {
   rect,
   wave,
   battery,
   star,
+  favorite,
+  hide,
   'arrow-left': arrowLeft,
   'theme-switcher': themeSwitcher,
+  'dummy-user': dummyUser,
 };
